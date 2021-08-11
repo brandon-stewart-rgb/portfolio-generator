e59ac849d80fd4a6925389239cac23b4976ee7c6
brandon-stewart-rgb <brandon@pixeltree.us>
2021-08-11 13:25:44 -0600

    generate html file

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
-// constant (const) can not be reassigned a value: Once we declare it, we can never reassign it. 
-//For strings and numbers, the content cannot be altered, because the reference is to the content. 
-//For arrays and objects, however, the content can be altered, because the reference is to the container, not to the content.
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
-// Notice the lack of parentheses around the `profileDataArr` parameter? And 'let'
-// Using let and const, variables that are created within any block of code {} will not exist outside the block of code. 
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach((profileItem) =>  console.log(profileItem));
-  };
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
